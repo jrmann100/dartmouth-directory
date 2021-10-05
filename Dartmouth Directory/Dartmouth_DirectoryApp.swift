e693f6f66e18156c227d57a951e34072818815c6
Jordan Mann <jordaloo@gmail.com>
2021-10-04 20:44:02 -0400

    Initial Commit

---
+++ Dartmouth Directory/Dartmouth_DirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dartmouth_DirectoryApp.swift
+//  Dartmouth Directory
+//
+//  Created by Jordan Mann on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Dartmouth_DirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
