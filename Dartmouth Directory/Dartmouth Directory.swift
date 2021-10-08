dd3f4debe2f995554461ee52104a466617cf0f69
Jordan Mann <jordaloo@gmail.com>
2021-10-08 16:07:00 -0400

    backups++

--- Dartmouth Directory/Dartmouth Directory.swift
+++ Dartmouth Directory/Dartmouth Directory.swift
@@ -5,9 +5,40 @@
 //  Created by Jordan Mann on 10/4/21.
 //
 
+import Contacts
+import ContactsUI
 import SwiftUI
 
-public struct Lookup: Codable {
+// https://github.com/judomat/CNContactViewController-debug/blob/main/CNContactViewController-debug/CNContactViewController.swift
+struct ContactView: UIViewControllerRepresentable {
+    let contact: CNContact
+
+    class Coordinator: NSObject, CNContactViewControllerDelegate {
+        func contactViewController(_ viewController: CNContactViewController, didCompleteWith contact: CNContact?) {}
+    }
+
+    init(_ contact: CNContact) {
+        self.contact = contact
+    }
+
+    func makeUIViewController(context: Context) -> CNContactViewController {
+        let controller = CNContactViewController(forUnknownContact: contact)
+        controller.delegate = context.coordinator
+        // todo: these don't seem to do anything
+        controller.allowsEditing = false
+        controller.allowsActions = false
+
+        return controller
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+
+    func updateUIViewController(_ uiViewController: CNContactViewController, context: Context) {}
+}
+
+struct Lookup: Codable {
     public let truncated: Bool
     public let users: [User]
 
@@ -22,17 +53,54 @@ public struct Lookup: Codable {
         public let telephoneNumber: String?
         public let uid: String
         public var id: String { uid }
+
+        public func asContact() -> CNMutableContact {
+            let contact = CNMutableContact()
+
+            // Store the profile picture as data
+            let image = UIImage(named: "Pine")
+            contact.imageData = image?.jpegData(compressionQuality: 1.0)
+
+            contact.givenName = displayName
+
+            if dcDeptclass != nil {
+                contact.jobTitle = dcDeptclass!
+            }
+
+            contact.emailAddresses = [CNLabeledValue(label: eduPersonPrimaryAffiliation == "Student" ? CNLabelSchool : CNLabelWork, value: mail as NSString)]
+
+            if telephoneNumber != nil {
+                contact.phoneNumbers = [CNLabeledValue(
+                    label: CNLabelPhoneNumberMain,
+                    value: CNPhoneNumber(stringValue: telephoneNumber!))]
+            }
+
+            if eduPersonNickname != nil {
+                contact.nickname = eduPersonNickname!
+            }
+
+            return contact
+
+//            // Save the newly created contact
+//            let store = CNContactStore()
+//            let saveRequest = CNSaveRequest()
+//            saveRequest.add(contact, toContainerWithIdentifier: nil)
+//
+//            do {
+//                try store.execute(saveRequest)
+//            } catch {
+//                print("Saving contact failed, error: \(error)")
+//                // Handle the error
+//            }
+        }
     }
 }
 
 struct ContentView: View {
-    @State var search: String
-    @State var users: [Lookup.User]
-
-    init() {
-        _search = .init(initialValue: "Jordan")
-        _users = .init(initialValue: [])
-    }
+    @State var search: String = "Jordan"
+    @State var lastSearch: String = ""
+    @State var users: [Lookup.User] = []
+    @State var selected: [Lookup.User] = []
 
     func lookup(_ search: String) async -> [Lookup.User] {
         if search == "" { return [] }
@@ -48,12 +116,6 @@ struct ContentView: View {
         }
     }
 
-    //                Picker(selection: .constant(1), label: Text("Picker"), content: {
-    //                    Text("'25s").tag(1)
-    //                    Text("Undergraduate").tag(2)
-    //                })
-    //                    .pickerStyle(SegmentedPickerStyle())
-
     private enum Field: Int, Hashable {
         case text
     }
@@ -61,7 +123,7 @@ struct ContentView: View {
     @FocusState private var focusedField: Field?
 
     // https://stackoverflow.com/q/58200555/9068081
-    struct ClearButton: ViewModifier {
+     struct ClearButton: ViewModifier {
         @Binding var text: String
         var focused: Bool
 
@@ -85,29 +147,27 @@ struct ContentView: View {
     struct UserView: View {
         let user: Lookup.User
         var body: some View {
-            VStack(alignment: .leading) {
-                if user.dcDeptclass != nil {
-                    Text(user.dcDeptclass!)
-                }
-
-                Text(user.mail)
-                Spacer()
-            }
-            .frame(maxWidth: .infinity, alignment: .topLeading)
-            .padding()
+            ContactView(user.asContact())
         }
     }
 
+    
+    static func mail(_ address: String) {
+        UIApplication.shared.open(URL(string: "mailto:\(address)")!)
+    }
+    
+    static func copy(_ text: String) {
+        UIPasteboard.general.setValue(text, forPasteboardType: "public.plain-text")
+    }
+    
     struct UserListItemView: View {
-        func copy(_ text: String) {
-            UIPasteboard.general.setValue(text, forPasteboardType: "public.plain-text")
-        }
 
         let user: Lookup.User
         var body: some View {
             NavigationLink {
                 UserView(user: user)
                     .navigationTitle(user.displayName)
+                    .navigationBarTitleDisplayMode(.inline)
             } label: {
                 HStack {
                     Text(user.displayName + ",").fontWeight(.medium)
@@ -116,6 +176,11 @@ struct ContentView: View {
                 .lineLimit(1)
             }
             .contextMenu {
+                Button {
+                    mail(user.mail)
+                } label: {
+                    Label("Send email", systemImage: "square.and.pencil")
+                }
                 Button {
                     copy(user.mail)
                 } label: {
@@ -128,48 +193,72 @@ struct ContentView: View {
                         Label("Copy mail address", systemImage: "tray.fill")
                     }
                 }
-                Button {
-                    print("save contact")
+// todo: save contact?
+            }
+        }
+    }
+    
+    struct SelectedItemsView: View {
+        @Binding var selected: [Lookup.User]
+        var body: some View {
+            Section {
+                ForEach(selected) { user in
+                    UserListItemView(user: user)
+        
+                        .swipeActions(edge: .leading) {
+                            Button (role: .destructive) { selected.remove(at: selected.firstIndex(of: user)!); return } label: { Image(systemName: "text.badge.plus") }
+                        }
+                        .swipeActions(edge: .trailing) {
+                            Button (role: .destructive) { selected.remove(at: selected.firstIndex(of: user)!); return } label: { Image(systemName: "text.badge.plus") }
+                        }
+                }
+            } header: {
+                HStack {
+                Label("Selected", systemImage: "text")
+                Menu {
+                    Button { print("1") } label: {Label("1", systemImage: "text")}
                 } label: {
-                    Label("Save contact", systemImage: "person.crop.circle.badge.plus")
+                    Label("Export", systemImage: "square.and.arrow.up" )
+                }
                 }
             }
+
         }
     }
 
     var body: some View {
         NavigationView {
             VStack {
-//                Spacer().frame(height: 20)
                 List {
-//                    Section {
-//                        TextField("Name", text: $search)
-//
-//                            .font(.headline)
-//                            .submitLabel(.search)
-//                            .focused($focusedField, equals: .text)
-//                            .modifier(ClearButton(text: $search, focused: focusedField == .text))
-//                            .minimumScaleFactor(0.75)
-//                            .onAppear {
-//                                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) { focusedField = .text }
-//                            }
-//                    }
-                    Section {
-                        ForEach(users) { user in
-                            UserListItemView(user: user)
-                        }
-                    } header: { Text("results") }
+                    if (selected.count > 0) {
+                        SelectedItemsView(selected: $selected)
+                    }
+                    ForEach(users.filter {!selected.contains($0)}) { user in
+                        UserListItemView(user: user)
+                            .swipeActions(edge: .leading) {
+                                Button { selected.append(user) } label: { Image(systemName: "text.badge.plus") }.tint(.green)
+                            }
+                            .swipeActions(edge: .trailing) {
+                                Button { mail() } label: { Image(systemName: "envelope.fill") }.tint(.blue)
+                            }
+                    }
                 }
                 .listStyle(.insetGrouped)
                 .animation(.default, value: users)
-                .refreshable {
-                    users = await lookup(search)
-                }
-                .task(id: search) {
-                    users = await lookup(search)
-                }
-                .searchable(text: $search, placement: .navigationBarDrawer(displayMode: .always), prompt: "Name")
-            }.navigationTitle("Lookup")
+                .animation(.default, value: selected) // todo ?
+            }
+            .navigationTitle("Lookup")
+            .refreshable {
+                users = []
+                users = await lookup(search)
+            }
+            .task(id: search) {
+                if search == lastSearch { return }
+                lastSearch = search
+                users = []
+                users = await lookup(search)
+            }
+            .searchable(text: $search, placement: .navigationBarDrawer(displayMode: .always), prompt: "Name")
         }
     }
 }
